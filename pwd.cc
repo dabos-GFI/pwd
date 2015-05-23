4dc8c15ff5a23a2790bc3bfee1fdb7c7913a1375
Damien Abos <damien.abos@gfi.fr>
2015-05-23 23:18:37 +0200

    Fix compilation on node v0.12+

--- pwd.cc
+++ pwd.cc
@@ -1,20 +1,21 @@
+// pwd.cc
 #include <node.h>
-#include <v8.h>
 #include <sys/types.h>
 #include <pwd.h>
 
+namespace pwd {
+
 using namespace v8;
+  
+Local<Object> mkpasswd(Isolate* isolate, const struct passwd *pwd) {
+  Local<Object> obj = Object::New(isolate);
+  obj->Set(String::NewFromUtf8(isolate, "pw_name"), String::NewFromUtf8(isolate, pwd->pw_name));
+  obj->Set(String::NewFromUtf8(isolate, "pw_uid"), Number::New(isolate, pwd->pw_uid));
+  obj->Set(String::NewFromUtf8(isolate, "pw_gid"), Number::New(isolate, pwd->pw_gid));
+  obj->Set(String::NewFromUtf8(isolate, "pw_dir"), String::NewFromUtf8(isolate, pwd->pw_dir));
+  obj->Set(String::NewFromUtf8(isolate, "pw_shell"), String::NewFromUtf8(isolate, pwd->pw_shell));
 
-
-Local<Object> mkpasswd(const struct passwd *pwd) {
-  Local<Object> obj = Object::New();
-  obj->Set(String::NewSymbol("pw_name"), String::New(pwd->pw_name));
-  obj->Set(String::NewSymbol("pw_uid"), Number::New(pwd->pw_uid));
-  obj->Set(String::NewSymbol("pw_gid"), Number::New(pwd->pw_gid));
-  obj->Set(String::NewSymbol("pw_dir"), String::New(pwd->pw_dir));
-  obj->Set(String::NewSymbol("pw_shell"), String::New(pwd->pw_shell));
-
-  // NOT SUPPORTED IN V1.0.0
+  // NOT SUPPORTED IN V1.1.0
   //  pw_passwd - encrypted password
   //  pw_change - password change time
   //  pw_class  - user access class
@@ -24,61 +25,56 @@ Local<Object> mkpasswd(const struct passwd *pwd) {
   return obj;
 }
 
-Handle<Value> Getpwuid(const Arguments& args) {
-  HandleScope scope;
-
-  if (args.Length() < 0) {
-    ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-    return scope.Close(Undefined());
+void Getpwuid(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  
+  if (args.Length() < 1) {
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
   }
 
   if (!args[0]->IsNumber()) {
-    ThrowException(Exception::TypeError(String::New("Argument must be a number")));
-    return scope.Close(Undefined());
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Argument must be a number")));
+    return;
   }
 
   uid_t uid = args[0]->NumberValue();
 
   struct passwd *pwd = getpwuid(uid);
 
-  if (pwd == NULL) {
-    return scope.Close(Undefined());
-  } else {
-    return scope.Close(mkpasswd(pwd));
+  if (pwd != NULL) {
+    args.GetReturnValue().Set(mkpasswd(isolate, pwd));
   }
 }
-
-
-Handle<Value> Getpwnam(const Arguments& args) {
-  HandleScope scope;
-
-  if (args.Length() < 0) {
-    ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-    return scope.Close(Undefined());
+  
+void Getpwnam(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = args.GetIsolate();
+  
+  if (args.Length() < 1) {
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
   }
 
   if (!args[0]->IsString()) {
-    ThrowException(Exception::TypeError(String::New("Argument must be a string")));
-    return scope.Close(Undefined());
+    isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Argument must be a string")));
+    return;
   }
 
   String::Utf8Value name(args[0]);
 
   struct passwd *pwd = getpwnam(*name);
 
-  if (pwd == NULL) {
-    return scope.Close(Undefined());
-  } else {
-    return scope.Close(mkpasswd(pwd));
+  if (pwd != NULL) {
+    args.GetReturnValue().Set(mkpasswd(isolate, pwd));
   }
 }
 
-void init(Handle<Object> exports) {
-  exports->Set(String::NewSymbol("getpwuid"),
-    FunctionTemplate::New(Getpwuid)->GetFunction());
-  exports->Set(String::NewSymbol("getpwnam"),
-    FunctionTemplate::New(Getpwnam)->GetFunction());
+void Init(Local<Object> exports, Local<Object> module) {
+  NODE_SET_METHOD(exports, "getpwuid", Getpwuid);
+  NODE_SET_METHOD(exports, "getpwnam", Getpwnam);
 }
 
-NODE_MODULE(pwd, init)
+NODE_MODULE(addon, Init)
+
+}  // namespace pwd
 
